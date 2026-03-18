52f0df641473e1afdea89a0035bfd41066d71bb3
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-03-18 09:15:27 +0530

    Add files via upload

---
+++ Week-5/Day-22/Set-2/Problem1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class StackUndo
+    {
+        string[] stack;
+        int top;
+        int size;
+
+        public StackUndo(int size)
+        {
+            this.size = size;
+            stack = new string[size];
+            top = -1;
+        }
+
+        // PUSH Operation (Add Action)
+        public void Push(string action)
+        {
+            if (top == size - 1)
+            {
+                Console.WriteLine("Stack Overflow (No more actions can be stored)");
+                return;
+            }
+
+            top++;
+            stack[top] = action;
+            Console.WriteLine("Action Added: " + action);
+            Display();
+        }
+
+        // POP Operation (Undo Action)
+        public void Pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack Underflow (Nothing to undo)");
+                return;
+            }
+
+            Console.WriteLine("Undo Action: " + stack[top]);
+            top--;
+            Display();
+        }
+
+        // Display Current State
+        public void Display()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Current State: Empty");
+                return;
+            }
+
+            Console.WriteLine("Current State After Operation:");
+            for (int i = 0; i <= top; i++)
+            {
+                Console.WriteLine(stack[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StackUndo editor = new StackUndo(5);
+
+            editor.Push("Type A");
+            editor.Push("Type B");
+            editor.Push("Type C");
+
+            editor.Pop();
+            editor.Pop();
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
