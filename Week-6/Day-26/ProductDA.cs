ea4ba1e61223e29c39772ee7e031b7a058e1d89f
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-03-24 18:22:44 +0530

    Add files via upload

---
+++ Week-6/Day-26/ProductDA.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace ConsoleApp2
+{
+    public class ProductDA
+    {
+        string connStr;
+
+        public ProductDA()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            connStr = config.GetConnectionString("DefaultConnection");
+        }
+
+        // INSERT
+        public void InsertProduct(string name, string category, decimal price)
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand cmd = new SqlCommand("sp_InsertProduct", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter p1 = new SqlParameter("@ProductName", SqlDbType.VarChar, 100);
+            p1.Value = name;
+
+            SqlParameter p2 = new SqlParameter("@Category", SqlDbType.VarChar, 50);
+            p2.Value = category;
+
+            SqlParameter p3 = new SqlParameter("@Price", SqlDbType.Decimal);
+            p3.Value = price;
+
+            cmd.Parameters.Add(p1);
+            cmd.Parameters.Add(p2);
+            cmd.Parameters.Add(p3);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            Console.WriteLine("Product Inserted");
+        }
+
+        // VIEW
+        public void ViewProducts()
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand cmd = new SqlCommand("sp_GetAllProducts", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            conn.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine($"{dr["ProductId"]}  {dr["ProductName"]}  {dr["Category"]}  {dr["Price"]}");
+            }
+
+            conn.Close();
+        }
+
+        // UPDATE
+        public void UpdateProduct(int id, string name, string category, decimal price)
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand cmd = new SqlCommand("sp_UpdateProduct", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter p1 = new SqlParameter("@ProductId", SqlDbType.Int);
+            p1.Value = id;
+
+            SqlParameter p2 = new SqlParameter("@ProductName", SqlDbType.VarChar, 100);
+            p2.Value = name;
+
+            SqlParameter p3 = new SqlParameter("@Category", SqlDbType.VarChar, 50);
+            p3.Value = category;
+
+            SqlParameter p4 = new SqlParameter("@Price", SqlDbType.Decimal);
+            p4.Value = price;
+
+            cmd.Parameters.Add(p1);
+            cmd.Parameters.Add(p2);
+            cmd.Parameters.Add(p3);
+            cmd.Parameters.Add(p4);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            Console.WriteLine("Product Updated");
+        }
+
+        // DELETE
+        public void DeleteProduct(int id)
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand cmd = new SqlCommand("sp_DeleteProduct", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter p1 = new SqlParameter("@ProductId", SqlDbType.Int);
+            p1.Value = id;
+
+            cmd.Parameters.Add(p1);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            Console.WriteLine("Product Deleted");
+        }
+
+            public void GetProductById(int id)
+            {
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand cmd = new SqlCommand("sp_GetProductDetailsById", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter p1 = new SqlParameter("@ProductId", SqlDbType.Int);
+            p1.Value = id;
+
+            cmd.Parameters.Add(p1);
+
+            conn.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                Console.WriteLine("\nProduct Found:");
+                Console.WriteLine($"Id : {dr["ProductId"]}");
+                Console.WriteLine($"Name : {dr["ProductName"]}");
+                Console.WriteLine($"Category : {dr["Category"]}");
+                Console.WriteLine($"Price : {dr["Price"]}");
+            }
+            else
+            {
+                Console.WriteLine("Product Details Not Found");
+            }
+
+            conn.Close();
+        }
+    }
+    }
---
+++ Week-6/Day-26/Program_file.cs
@@ -0,0 +1,97 @@
+﻿using ConsoleApp2;
+
+    class Program
+    {
+        static void Main()
+        {
+            ProductDA dal = new ProductDA();
+
+            while (true)
+            {
+                Console.WriteLine("\n1.Insert\n2.View\n3.Update\n4.Delete\n5.Get Details By ID \n6.Exit");
+
+                if (!int.TryParse(Console.ReadLine(), out int ch))
+                {
+                    Console.WriteLine("Invalid Choice");
+                    continue;
+                }
+
+            if (ch == 1)
+            {
+                Console.Write("\nProduct Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Category: ");
+                string cat = Console.ReadLine();
+
+                Console.Write("Price: ");
+                if (!decimal.TryParse(Console.ReadLine(), out decimal price))
+                {
+                    Console.WriteLine("Invalid Price");
+                    continue;
+                }
+
+                dal.InsertProduct(name, cat, price);
+            }
+            else if (ch == 2)
+            {
+                dal.ViewProducts();
+            }
+            else if (ch == 3)
+            {
+                Console.Write("Id: ");
+                if (!int.TryParse(Console.ReadLine(), out int id))
+                {
+                    Console.WriteLine("Invalid Id");
+                    continue;
+                }
+
+                Console.Write("Product Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Category: ");
+                string cat = Console.ReadLine();
+
+                Console.Write("Price: ");
+                if (!decimal.TryParse(Console.ReadLine(), out decimal price))
+                {
+                    Console.WriteLine("Invalid Price");
+                    continue;
+                }
+
+                dal.UpdateProduct(id, name, cat, price);
+            }
+            else if (ch == 4)
+            {
+                Console.Write("Enter Id: ");
+                if (!int.TryParse(Console.ReadLine(), out int id))
+                {
+                    Console.WriteLine("Invalid Id");
+                    continue;
+                }
+
+                dal.DeleteProduct(id);
+            }
+            else if (ch == 5)
+            {
+                Console.Write("Enter Product Id: ");
+
+                if (!int.TryParse(Console.ReadLine(), out int id))
+                {
+                    Console.WriteLine("Invalid Id");
+                    continue;
+                }
+
+                dal.GetProductById(id);
+            }
+            else if (ch == 6)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Wrong Choice");
+            }
+            }
+        }
+    }
