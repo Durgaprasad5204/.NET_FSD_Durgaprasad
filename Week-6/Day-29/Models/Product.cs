170c95aae935c9715323d595192e72778bea5803
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-03-30 09:12:20 +0530

    Add files via upload

---
+++ Week-6/Day-29/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+       
+        public int Id { get; set; }
+
+        
+        
+        public string Name { get; set; }
+
+        
+        public double Price { get; set; }
+
+      
+        public string Category { get; set; }
+    }
+}
