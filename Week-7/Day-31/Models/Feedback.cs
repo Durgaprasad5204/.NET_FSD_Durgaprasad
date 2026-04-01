4ac8d1e8d546fb9e31a625026b95684cfc1c6c0a
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-04-01 09:17:54 +0530

    Add files via upload

---
+++ Week-7/Day-31/Models/Feedback.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Feedback
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Comments { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+    }
+}
