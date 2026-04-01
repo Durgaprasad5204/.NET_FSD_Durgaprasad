9f17c4d7d52725eb2a093eceef093ba3411ee4e0
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-04-01 19:17:42 +0530

    Add files via upload

---
+++ Week-7/Day-32/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class ApplicationDbContext:DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
