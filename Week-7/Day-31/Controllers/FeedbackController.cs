39ac74bb86ac6cb120cf787a2f666eb0238f068f
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-04-01 09:18:26 +0530

    Add files via upload

---
+++ Week-7/Day-31/Controllers/FeedbackController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("feedback")]
+    public class FeedbackController : Controller
+    {
+       
+        [HttpGet("form")]
+        public IActionResult FeedbackForm()
+        {
+            return View();
+        }
+
+        
+        [HttpPost("submit")]
+        public IActionResult SubmitFeedback(Feedback feedback)
+        {
+            if (feedback.Rating >= 4)
+            {
+                ViewData["Message"] = "Thank You for your positive feedback!";
+            }
+            else
+            {
+                ViewData["Message"] = "We will improve based on your feedback.";
+            }
+
+            return View("FeedbackForm");
+        }
+    }
+}
---
+++ Week-7/Day-31/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers
+{
+    [Route("student")]
+    public class StudentController:Controller
+    {
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(string studentName, int age, string course)
+        {
+            TempData["StudentName"] = studentName;
+            TempData["Age"] = age;
+            TempData["Course"] = course;
+
+            return RedirectToAction("Display");
+        }
+
+        [HttpGet("Display")]
+        public IActionResult Display()
+        { 
+            ViewBag.StudentName = TempData["StudentName"];
+            ViewBag.Age = TempData["Age"];
+            ViewBag.Course = TempData["Course"];
+            return View();
+        }
+    }
+}
