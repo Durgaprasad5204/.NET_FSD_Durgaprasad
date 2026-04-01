839abd7524307d65ce254ceddb51d0833be80622
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-04-01 19:11:45 +0530

    Add files via upload

---
+++ Week-7/Day-32/Services/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public interface IMovieService
+    {
+        IEnumerable<Movie> GetAll();
+        Movie GetById(int id);
+        void Add(Movie movie);
+        void Update(Movie movie);
+        void Delete(int id);
+    }
+}
---
+++ Week-7/Day-32/Services/MovieService.cs
@@ -0,0 +1,41 @@
+﻿using WebApplication2.Models;
+using WebApplication2.Repositories;
+using WebApplication2.Services;
+
+namespace WebApplication2.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _repository;
+
+        public MovieService(IMovieRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public Movie GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void Add(Movie movie)
+        {
+            _repository.Add(movie);
+        }
+
+        public void Update(Movie movie)
+        {
+            _repository.Update(movie);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
