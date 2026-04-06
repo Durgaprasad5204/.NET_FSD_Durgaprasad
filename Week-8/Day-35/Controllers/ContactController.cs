59c30670d0c1b5d1d88cec6b53da6386db9e13ba
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-04-06 18:15:08 +0530

    Add files via upload

---
+++ Week-8/Day-35/Controllers/ContactController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication3.Models;
+using WebApplication3.Services;
+
+namespace WebApplication3.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _service;
+
+        public ContactController(IContactService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_service.GetAll());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var contactObj = _service.GetById(id);
+            return View(contactObj);
+        }
+
+        public IActionResult Create()
+        {
+            LoadDropdowns();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ContactInfo contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Add(contact);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                LoadDropdowns();
+                ViewBag.ErrorMessage = "Invalid Contact details.";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            LoadDropdowns();
+            var contactObj = _service.GetById(id);
+            return View(contactObj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ContactInfo contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Update(contact);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                LoadDropdowns();
+                ViewBag.ErrorMessage = "Invalid Contact details.";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var contactObj = _service.GetById(id);
+            return View(contactObj);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var contactObj = _service.GetById(id);
+
+            if (contactObj != null)
+            {
+                _service.Delete(id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Requested contact does not exist.";
+                return View();
+            }
+        }
+
+        private void LoadDropdowns()
+        {
+            ViewBag.Companies = new SelectList(
+                _service.GetCompanies(),
+                "CompanyId",
+                "CompanyName");
+
+            ViewBag.Departments = new SelectList(
+                _service.GetDepartments(),
+                "DepartmentId",
+                "DepartmentName");
+        }
+    }
+}
