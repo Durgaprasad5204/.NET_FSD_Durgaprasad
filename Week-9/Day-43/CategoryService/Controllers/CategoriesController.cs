7adc66c2d6a1c33c21ce8aeef1a95452c01f795e
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-04-17 00:07:59 +0530

    Add files via upload

---
+++ Week-9/Day-43/CategoryService/Controllers/CategoriesController.cs
@@ -0,0 +1,53 @@
+﻿using CategoryService.Models;
+using CategoryService.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _service;
+
+        public CategoriesController(ICategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_service.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_service.GetById(id));
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            return Ok(_service.Add(category));
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        public IActionResult Update(Category category)
+        {
+            return Ok(_service.Update(category));
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok(_service.Delete(id));
+        }
+    }
+}
