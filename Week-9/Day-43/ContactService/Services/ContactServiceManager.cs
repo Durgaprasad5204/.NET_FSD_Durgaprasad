d862f4b68541ef57467bb6cd36b3ea126ea3c7ab
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-04-17 00:06:15 +0530

    Add files via upload

---
+++ Week-9/Day-43/ContactService/Services/ContactServiceManager.cs
@@ -0,0 +1,25 @@
+﻿using ContactService.Models;
+using ContactService.Repositories;
+
+namespace ContactService.Services
+{
+    public class ContactServiceManager : IContactService
+    {
+        private readonly IContactRepository _repo;
+
+        public ContactServiceManager(IContactRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<Contact> GetAll() => _repo.GetAll();
+
+        public Contact GetById(int id) => _repo.GetById(id);
+
+        public Contact Add(Contact contact) => _repo.Add(contact);
+
+        public Contact Update(Contact contact) => _repo.Update(contact);
+
+        public bool Delete(int id) => _repo.Delete(id);
+    }
+}
