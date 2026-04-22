63516281e0f5d6deeca597a68e756cccf727f65a
Durgaprasad Manukonda <155614802+Durgaprasad5204@users.noreply.github.com>
2026-04-22 15:53:18 +0530

    Add files via upload

---
+++ Week-10/Day-46/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication7.Models;
+
+namespace WebApplication7.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(int id);
+    }
+}
